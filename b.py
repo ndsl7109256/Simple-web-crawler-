1617c87f22441be03d1aa4b084e539f9d569c156
BK_0710 <ndsl7109256@gmail.com>
2019-04-23 01:23:32 +0800

    Is there Name ambiguity problem?

---
+++ b.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import urllib.request;
+import re;
+
+author = "Ian+Goodfellow"
+url = "https://arxiv.org/search/?query=" + author + "&searchtype=author"
+content = urllib.request.urlopen(url)
+html_str = content.read().decode('utf-8')
+pattern = '<a href=\"/search/\?searchtype=author[\s\S]*?</a>'
+result = re.findall(pattern, html_str)
+
+
+names = []
+
+print("[ Author: " + author + " ]")
+for r in result:
+    co = r.split("\">")[1].split("</a>")[0].strip()
+    names.append(co)
+
+
+p = list(set(names))
+p.sort()
+
+author_name = author.replace("+"," ")
+
+
+for g in p:
+    if g != author_name:
+        print(' %s : %d times' %(g,names.count(g)))
+
+

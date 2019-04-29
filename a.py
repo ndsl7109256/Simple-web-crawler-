0789c8a478bcb81ab04bb0de2f472c3f9feb65c5
BK_0710 <ndsl7109256@gmail.com>
2019-04-29 19:42:34 +0800

    Clear some print

--- a.py
+++ a.py
@@ -2,10 +2,27 @@ import matplotlib.pyplot as plt
 import urllib.request;
 import re;
 
-author = "Ian+Goodfellow"
-url = "https://arxiv.org/search/?query=" + author + "&searchtype=author&size=200"
+#author = "Ian+Goodfellow"
+author = input("Input Author: ")
+author = author.replace(" ","+")
+
+#print(author)
+url = "https://arxiv.org/search/?query=" + author + "&searchtype=author"
 content = urllib.request.urlopen(url)
 html_str = content.read().decode('utf-8')
+
+page = html_str.count('pagination-link')/2
+#print(page)
+if page>=2.0:
+    for p in range(1,int(page)):
+#        print(p)
+        start = p*50
+        url2 = url + "&start="+str(start)
+        content = urllib.request.urlopen(url2)
+#        print(url2)
+        html_str = html_str + content.read().decode('utf-8')
+
+
 pattern = 'title is-5 mathjax[\s\S]*?</p>'
 result = re.findall(pattern, html_str)
 patten2 = 'originally announced[\s\S]*?</p>'
@@ -13,18 +30,16 @@ years = re.findall(patten2, html_str)
 
 count = 0
 
-print("[ Author: " + author + " ]")
+#print("[ Author: " + author + " ]")
 for r in result:
-    print(r)
     title = r.split("title is-5 mathjax\">")[1].split("</p>")[0].strip()
-    print(title)
     count = count + 1
 list =[]
 for g in years:
     date = g.split("originally announced</span>")[1].split("</p>")[0].strip()
     year = date.split(' ')[1].split('.')[0].strip()
-    print(date)
-    print(int(year))
+#    print(date)
+#    print(int(year))
     list.append(int(year))
 a = int(min(list))
 b = int(max(list))
@@ -39,7 +54,6 @@ plt.figure()
 plt.bar(x,y)
 plt.show()
 
-print(url)
-print(count)
+#print(url)
 
-
+#print(count)
--- b.py
+++ b.py
@@ -2,17 +2,36 @@ import matplotlib.pyplot as plt
 import urllib.request;
 import re;
 
-author = "Ian+Goodfellow"
-url = "https://arxiv.org/search/?query=" + author + "&searchtype=author&size=200"
+#author = "Ian+Goodfellow"
+
+author = input("Input Author: ")
+author = author.replace(" ","+")
+
+
+url = "https://arxiv.org/search/?query=" + author + "&searchtype=author"
 content = urllib.request.urlopen(url)
 html_str = content.read().decode('utf-8')
+
+
+page = html_str.count('pagination-link')/2
+#print(page)
+if page>=2.0:
+    for p in range(1,int(page)):
+#        print(p)
+        start = p*50
+        url2 = url + "&start="+str(start)
+        content = urllib.request.urlopen(url2)
+#        print(url2)
+        html_str = html_str + content.read().decode('utf-8')
+
+
 pattern = '<a href=\"/search/\?searchtype=author[\s\S]*?</a>'
 result = re.findall(pattern, html_str)
 
 
 names = []
 
-print("[ Author: " + author + " ]")
+#print("[ Author: " + author + " ]")
 for r in result:
     co = r.split("\">")[1].split("</a>")[0].strip()
     names.append(co)
